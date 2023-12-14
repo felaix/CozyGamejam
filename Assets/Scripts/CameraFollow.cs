2f734791d156c29f7b293b72b671427a961fb2ac
Felix <xfelixe@gmail.com>
2023-12-14 13:51:55 +0100

    camera bounds

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,15 +7,26 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] private Transform player;
     [SerializeField] private float smoothSpd = .125f;
     [SerializeField] private Vector3 offset = new(0f, -3.5f, 0f);
-    private float minY = -6.5f;
+    //private float minY = -6.5f;
+
+
+    [SerializeField] private float leftBoundary = -3f;
+    [SerializeField] private float rightBoundary = 3f;
+    [SerializeField] private float topBoundary = 3f;
+    [SerializeField] private float bottomBoundary = -3f;
 
     private void Update()
     {
         if (player == null) return;
 
-        Vector3 desiredPos = new(player.position.x, player.position.y, transform.position.z);
+
+        float clampedX = Mathf.Clamp(player.position.x, leftBoundary, rightBoundary);
+        float clampedY = Mathf.Clamp(player.position.y, bottomBoundary, topBoundary);
+
+
+        Vector3 desiredPos = new(clampedX, clampedY, transform.position.z);
         Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos + offset, smoothSpd);
-        smoothedPos.y = Mathf.Clamp(smoothedPos.y, minY, Mathf.Infinity);
+
         transform.position = smoothedPos;
     }
 }
